1405f6c2587b4f2e7042473079c27662ceb31a47
tianyunh <90026257+tianyunh@users.noreply.github.com>
2021-11-12 18:31:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ setup(
     name="csv-linter",
     description="lint csv files",
     packages=find_packages(),
-    author="Alfredo Deza",
+    author="Tianyun Hou",
     entry_points="""
     [console_scripts]
     csv-linter=csv_linter:main
     """,
     install_requires=["click==7.1.2", "pandas==1.2.0"],
-    version="0.0.1",
-    url="https://github.com/paiml/practical-mlops-book",
+    version="0.0.1"
 )
